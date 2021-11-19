0400ead722601e2961e82fad76928f645b01ab7d
Baihaki123 <baihakiaski@gmail.com>
2021-11-19 08:29:55 +0700

    gas keh 10 dina dadi

--- application/views/buku/daftarbuku.php
+++ application/views/buku/daftarbuku.php
@@ -13,18 +13,17 @@
     <h5 style="min-height:30px;"><?= $buku->pengarang ?></h5>
     <h5><?= $buku->penerbit ?></h5>
     <h5><?= substr($buku->tahun_terbit, 0, 4) ?></h5>
-<p>
-<?php
-if ($buku->stok < 1) {
-echo "<i class='btn btn-outlineprimary fas fw fa-shopping-cart'> Booking&nbsp;&nbsp 0</i>";
-} else {
-echo "<a class='btn btn-outlineprimary fas fw fa-shoppingcart' href='" . base_url('booking/tambahBooking/' . $buku->id) . "'> Booking</a>";
-}
-?>
-<a class="btn btn-outlinewarning fas fw fa-search" href="<?= base_url('home/detailBuku/' . $buku->id); ?>"> Detail</a></p>
+    <?php
+    if ($buku->stok < 1) {
+        echo "<i class='btn btn-outline-primary fas fw fa-shopping-cart mr-1'> Booking&nbsp;&nbsp 0</i>";
+    } else {
+        echo "<a class='btn btn-outline-primary fas fw fa-shopping-cart mr-1' href='" . base_url('booking/tambahBooking/' . $buku->id) . "'> Booking</a>";
+    } ?>
+        <a class="btn btn-outline-warning fas fw fa-search" href="<?= base_url('home/detailBuku/' . $buku->id); ?>"> Detail</a>
+    </div>
 </div>
-</div>
-</div> <?php } ?>
+</div> 
+<?php } ?>
 <!-- end looping -->
 </div>
 </div>
--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -6,9 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pustaka-Booking | <?= $judul; ?></title>
     <link rel="icon" type="image/png" href="<?= base_url('assets/img/logo/'); ?>logo-pb.png">
-    <link rel="stylesheet" href="<?= base_url('assets/'); ?>user/css/bootstrap.css">
-    <link href="<?= base_url('assets/'); ?>vendor/fontawesomefree/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="<?= base_url('assets/'); ?>datatable/datatables.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="<?= base_url('assets/user/css/bootstrap.css'); ?>">
+    <link href="<?= base_url('assets/vendor/fontawesome/css/all.css'); ?>" rel="stylesheet" type="text/css">
+    <link href="<?= base_url('assets/datatable/datatables.css'); ?>" rel="stylesheet" type="text/css">
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -19,7 +19,7 @@
         </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-    <a class="nav-item navlink active" href="<?= base_url(); ?>">Beranda <span class="sronly">(current)</span></a>
+    <a class="nav-item nav-link" href="<?= base_url(); ?>">Beranda</a>
     <?php
     if (!empty($this->session->userdata('email'))) { ?>
     <a class="nav-item navlink" href="<?= base_url('booking'); ?>">Booking <b><?= $this->ModelBooking->getDataWhere('temp', ['email_user' => $this->session->userdata('email')])->num_rows(); ?></b> Buku</a>
